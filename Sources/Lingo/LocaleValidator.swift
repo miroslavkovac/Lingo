cfb9f66785f26ec26243f3e71bf78afb728a446f
Miroslav Kovac <miroslav.kovac@icloud.com>
2017-09-04 21:48:30 +0200

    Fixed crash when accessing Locale.availableIdentifiers on Linux

--- Sources/Lingo/LocaleValidator.swift
+++ Sources/Lingo/LocaleValidator.swift
@@ -2,8 +2,16 @@ import Foundation
 
 class LocaleValidator {
     
-    private static let validLocaleIdentifiers = Set(Locale.availableIdentifiers)
-    
+    // On Linux `Locale.availableIdentifiers` throws an exception.
+    // Issue reported: https://bugs.swift.org/browse/SR-3634
+    // Pull request with fix: https://github.com/apple/swift-corelibs-foundation/pull/944
+    // This check can be removed when the changes are available in a new Swift version.
+    #if os(Linux)
+        private static let validLocaleIdentifiers = Set<String>()
+    #else
+        private static let validLocaleIdentifiers = Set(Locale.availableIdentifiers)
+    #endif
+
     /// Checks if given locale is present in Locale.availableIdentifiers
     func validate(locale: LocaleIdentifier) -> Bool {
         return LocaleValidator.validLocaleIdentifiers.contains(locale)
